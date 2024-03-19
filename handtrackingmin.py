0fd830e75dcb69fe9297c118f92940541bad125f
keshav-610 <kesavaprakash1610@gmail.com>
2024-03-19 20:04:55 +0530

    chapter 1 finished 19/3

--- handtrackingmin.py
+++ handtrackingmin.py
@@ -7,6 +7,9 @@ mphands=mp.solutions.hands
 hands=mphands.Hands()
 mpDraw=mp.solutions.drawing_utils
 
+ptime=0
+ctime=0
+
 while True:
     try:
         success,img=cap.read()
@@ -16,8 +19,25 @@ while True:
         
         if results.multi_hand_landmarks:
             for handlms in results.multi_hand_landmarks:
+                for id,lm in enumerate(handlms.landmark):
+                    #print(id,lm)
+                    h,w,c=img.shape
+                    cx,cy=int(lm.x*w),int(lm.y*h)
+                    print(id,cx,cy)
+                    if id==8:
+                        cv2.circle(img,(cx,cy),15,(255,0,0),cv2.FILLED)
+                    if id==4:
+                        cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+
                 mpDraw.draw_landmarks(img,handlms,mphands.HAND_CONNECTIONS)
-    
+
+
+        ctime=time.time()
+        fps=1/(ctime-ptime)
+        ptime=ctime
+
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,2,255),3)
+
         cv2.imshow("Image",img)
         cv2.waitKey(1)
     
