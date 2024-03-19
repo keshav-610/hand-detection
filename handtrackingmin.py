2074526b70709412925ef8247fdb8264ad83a08f
keshav-610 <kesavaprakash1610@gmail.com>
2024-03-19 20:16:54 +0530

    chapter 1 finished 19/3

--- handtrackingmin.py
+++ handtrackingmin.py
@@ -15,12 +15,10 @@ while True:
         success,img=cap.read()
         imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         results=hands.process(imgRGB)
-        #print(results.multi_hand_landmarks)
         
         if results.multi_hand_landmarks:
             for handlms in results.multi_hand_landmarks:
                 for id,lm in enumerate(handlms.landmark):
-                    #print(id,lm)
                     h,w,c=img.shape
                     cx,cy=int(lm.x*w),int(lm.y*h)
                     print(id,cx,cy)
